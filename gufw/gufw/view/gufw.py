46b62003dc15dbaba96f3880739ac951145be5a8
Jeremy Bícha <jeremy.bicha@canonical.com>
2024-01-24 19:30:22 -0500

    Switch from webkit2gtk 4.0 to 4.1

--- gufw/gufw/view/gufw.py
+++ gufw/gufw/view/gufw.py
@@ -18,7 +18,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
-gi.require_version('WebKit2', '4.0')
+gi.require_version('WebKit2', '4.1')
 from gi.repository import Gtk, Gdk, WebKit2
 from string import Template
 
